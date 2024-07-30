dc822a17f763aaf8957ae518dd083ebc0a0410b4
Felipe <f.queiroz@bubup.com.br>
2024-07-30 10:47:44 -0300

    Mais Informacoes - Historico de Corridas

--- lib/pages/trips_history_page.dart
+++ lib/pages/trips_history_page.dart
@@ -62,69 +62,72 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
               if (tripsList[index]["status"] != null &&
                   tripsList[index]["status"] == "ended" &&
                   tripsList[index]["userID"] == FirebaseAuth.instance.currentUser!.uid) {
-                return Card(
-                  color: Colors.white,
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // pickup - fare amount
-                        Row(
-                          children: [
-                            Image.asset('assets/images/initial.png', height: 16, width: 16),
+                return GestureDetector(
+                  onTap: () => showTripDetailsDialog(context, tripsList[index]),
+                  child: Card(
+                    color: Colors.white,
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // pickup - fare amount
+                          Row(
+                            children: [
+                              Image.asset('assets/images/initial.png', height: 16, width: 16),
 
-                            const SizedBox(width: 18),
+                              const SizedBox(width: 18),
 
-                            Expanded(
-                              child: Text(
-                                tripsList[index]["pickUpAddress"].toString(),
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black,
+                              Expanded(
+                                child: Text(
+                                  tripsList[index]["pickUpAddress"].toString(),
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
                                 ),
                               ),
-                            ),
 
-                            const SizedBox(width: 5),
+                              const SizedBox(width: 5),
 
-                            Text(
-                              "\$ " + tripsList[index]["fareAmount"].toString(),
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ],
-                        ),
-
-                        const SizedBox(height: 8),
-
-                        // dropoff
-                        Row(
-                          children: [
-                            Image.asset('assets/images/final.png', height: 16, width: 16),
-
-                            const SizedBox(width: 18),
-
-                            Expanded(
-                              child: Text(
-                                tripsList[index]["dropOffAddress"].toString(),
-                                overflow: TextOverflow.ellipsis,
+                              Text(
+                                "\$ " + tripsList[index]["fareAmount"].toString(),
                                 style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black,
+                                  fontSize: 16,
+                                  color: Colors.blue,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+
+                          const SizedBox(height: 8),
+
+                          // dropoff
+                          Row(
+                            children: [
+                              Image.asset('assets/images/final.png', height: 16, width: 16),
+
+                              const SizedBox(width: 18),
+
+                              Expanded(
+                                child: Text(
+                                  tripsList[index]["dropOffAddress"].toString(),
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -135,6 +138,83 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
           );
         },
       ),
+    );
+  }
+
+  void showTripDetailsDialog(BuildContext context, Map tripDetails) async {
+    String driverID = tripDetails["driverID"];
+    DatabaseReference driverRef = FirebaseDatabase.instance.ref().child("drivers").child(driverID);
+    DataSnapshot snapshot = await driverRef.once().then((event) => event.snapshot);
+
+    String driverName = "";
+    String driverEmail = "";
+    if (snapshot.value != null) {
+      Map driverData = snapshot.value as Map;
+      driverName = driverData["name"];
+      driverEmail = driverData["email"] ?? "Não disponível";
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: BorderSide(color: Colors.blue.shade900, width: 2),
+          ),
+          title: const Text(
+            "Detalhes da Viagem",
+            style: TextStyle(color: Colors.black),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  "Partida: ${tripDetails["pickUpAddress"]}",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Destino: ${tripDetails["dropOffAddress"]}",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Valor: \$${tripDetails["fareAmount"]}",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Motorista: $driverName",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Email do Motorista: $driverEmail",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Data e Horário: ${tripDetails["publishDateTime"]}",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                "Fechar",
+                style: TextStyle(color: Colors.blue),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
